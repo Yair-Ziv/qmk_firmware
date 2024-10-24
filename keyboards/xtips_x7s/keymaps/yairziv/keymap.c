80d3a2e777b57f4d5d7347e4372810b444a41d1b
Yair Ziv <yair.ziv@1mrobotics.com>
2024-10-25 01:56:04 +0300

    Added qwerty layer

--- keyboards/xtips_x7s/keymaps/yairziv/keymap.c
+++ keyboards/xtips_x7s/keymaps/yairziv/keymap.c
@@ -3,25 +3,32 @@
 
 #include QMK_KEYBOARD_H
 
+/*
+ * Layers
+ */
+enum layers {
+    _QWERTY,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-     /*
-      * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
-      * │ Q │ W │ E │ R │ T │       │ Y │ U │ I │ O │ P │
-      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
-      * │ A │ S │ D │ F │ G │       │ H │ J │ K │ L │ ; │
-      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
-      * │ Z │ X │ C │ V │ B │       │ N │ M │ , │ . │ / │
-      * └───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┘
-      *           ┌───┐                   ┌───┐
-      *           │GUI├───┐           ┌───┤Alt│
-      *           └───┤Bsp├───┐   ┌───┤Ent├───┘
-      *               └───┤   │   │   ├───┘
-      *                   └───┘   └───┘
-      */
-    [0] = LAYOUT_split_3x5_3(
-        KC_Q,       KC_W,       KC_E,       KC_R, KC_T,                    KC_Y,    KC_U, KC_I,       KC_O,       KC_P,
-        LCTL(KC_A), LOPT(KC_S), LGUI(KC_D), KC_F, KC_G,                    KC_H,    KC_J, RGUI(KC_K), ROPT(KC_L), RCTL(KC_SCLN),
-        KC_Z,       KC_X,       KC_C,       KC_V, KC_B,                    KC_N,    KC_M, KC_COMM,    KC_DOT,     KC_SLSH,
-                                   KC_LGUI, KC_BSPC, KC_SPC,           KC_SPC,  KC_ENT,  KC_RALT
-    )
+ /*
+  * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
+  * │ Q │ W │ E │ R │ T │       │ Y │ U │ I │ O │ P │
+  * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
+  * │ A │ S │ D │ F │ G │       │ H │ J │ K │ L │ ; │
+  * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
+  * │ Z │ X │ C │ V │ B │       │ N │ M │ , │ . │ / │
+  * └───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┘
+  *           ┌───┐                   ┌───┐
+  *           │GUI├───┐           ┌───┤Alt│
+  *           └───┤Bsp├───┐   ┌───┤Ent├───┘
+  *               └───┤   │   │   ├───┘
+  *                   └───┘   └───┘
+  */
+[_QWERTY] = LAYOUT_split_3x5_3(
+KC_Q,         KC_W,       KC_E,       KC_R,       KC_T,        /*||*/           KC_Y,    KC_U,       KC_I,       KC_O,             KC_P,
+LGUI_T(KC_A), LOPT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,     /*||*/              KC_H,    RSFT_T(KC_J), RCTL_T(KC_K), ROPT_T(KC_L),       RGUI_T(KC_SCLN),
+KC_Z,       LOPT_T(KC_X), KC_C,       KC_V,       KC_B,       /*||*/            KC_N,    KC_M,       KC_COMM,    ROPT_T(KC_DOT),     KC_SLSH,
+                       KC_LGUI, KC_BSPC, LT(_SYM, KC_SPC),     /*||*/      LT(_NUM, KC_ENT),  KC_ENT,  KC_RALT
+),
 };
