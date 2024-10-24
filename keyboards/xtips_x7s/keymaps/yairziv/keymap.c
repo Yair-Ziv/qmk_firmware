147319a3905e0f61dd92d6f7d983640467e75c8e
Yair Ziv <yair.ziv@1mrobotics.com>
2024-10-25 02:31:03 +0300

    Set enter as layer-tap and made the next key delete

--- keyboards/xtips_x7s/keymaps/yairziv/keymap.c
+++ keyboards/xtips_x7s/keymaps/yairziv/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 KC_Q,         KC_W,       KC_E,       KC_R,       KC_T,        /*||*/           KC_Y,    KC_U,       KC_I,       KC_O,             KC_P,
 LGUI_T(KC_A), LOPT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,     /*||*/              KC_H,    RSFT_T(KC_J), RCTL_T(KC_K), ROPT_T(KC_L),       RGUI_T(KC_SCLN),
 KC_Z,       LOPT_T(KC_X), KC_C,       KC_V,       KC_B,       /*||*/            KC_N,    KC_M,       KC_COMM,    ROPT_T(KC_DOT),     KC_SLSH,
-                       KC_LGUI, KC_BSPC, LT(_SYM, KC_SPC),     /*||*/      LT(_NUM, KC_ENT),  KC_ENT,  KC_RALT
+                       KC_LGUI, LT(_NAV, KC_BSPC), LT(_SYM, KC_SPC),     /*||*/      LT(_NUM, KC_ENT),  KC_DEL,  KC_RALT
 ),
 [_SYM] = LAYOUT_split_3x5_3(
 RSFT(KC_GRV),       RSFT(KC_COMM),       RSFT(KC_DOT),       LSFT(KC_6),       RSFT(KC_BSLS),         /*||*/          LSFT(KC_7),    KC_BSLS,       KC_LBRC,       KC_RBRC,  KC_NO,
