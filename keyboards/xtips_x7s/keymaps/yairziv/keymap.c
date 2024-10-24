45e59bddec9ea2acb17fd586c8921c4609953ec6
Yair Ziv <yair.ziv@1mrobotics.com>
2024-10-25 01:56:56 +0300

    Added numbers layer

--- keyboards/xtips_x7s/keymaps/yairziv/keymap.c
+++ keyboards/xtips_x7s/keymaps/yairziv/keymap.c
@@ -11,6 +11,7 @@
 enum layers {
     _QWERTY,
     _SYM,
+    _NUM
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -39,5 +40,11 @@ RSFT(KC_GRV),       RSFT(KC_COMM),       RSFT(KC_DOT),       LSFT(KC_6),       R
 KC_ESC, KC_MINS, LSFT(KC_EQL), KC_EQL, KC_QUOT,        /*||*/           RSFT(KC_QUOT),    KC_NO, RSFT(KC_9), RSFT(KC_0),       LSFT(KC_SCLN),
 KC_NO,       KC_SLSH, RSFT(KC_8),       LSFT(KC_4),       KC_NO,       /*||*/            KC_NO,    KC_NO,       RSFT(KC_LBRC),    LSFT(KC_RBRC),     KC_NO,
                        KC_LGUI, KC_BSPC, LT(_SYM, KC_SPC),      /*||*/     KC_SPC,  KC_ENT,  KC_RALT
+),
+[_NUM] = LAYOUT_split_3x5_3(
+LSFT(KC_1), LSFT(KC_2), LSFT(KC_3), LSFT(KC_4), LSFT(KC_5),        /*||*/               LSFT(KC_6), LSFT(KC_7), LSFT(KC_8),       LSFT(KC_9), LSFT(KC_0),
+KC_1, KC_2, KC_3, KC_4, KC_5,        /*||*/               KC_6, KC_7, KC_8,       KC_9, KC_0,
+KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,        /*||*/               KC_F6, KC_F7, KC_F8,       KC_F9, KC_F10,
+                       KC_NO, KC_NO, KC_NO,      /*||*/     KC_NO,  KC_NO,  KC_NO
 ),
 };
